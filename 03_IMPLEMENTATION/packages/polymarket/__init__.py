4c9cdd43b5aa97a5881d7601384e0780ecea4bf8
userist123 <nitumarius@yahoo.com>
2026-09-12 16:57:12 +0300

    Phase 11: export batch price history

--- 03_IMPLEMENTATION/packages/polymarket/__init__.py
+++ 03_IMPLEMENTATION/packages/polymarket/__init__.py
@@ -10,6 +10,7 @@ from .calibration import (
     score_calibration,
 )
 from .historical_paper_replay import (
+    CLOB_BATCH_PRICES_HISTORY_URL,
     CLOB_PRICES_HISTORY_URL,
     GAMMA_MARKETS_URL as HISTORICAL_GAMMA_MARKETS_URL,
     HISTORICAL_PAPER_REPLAY_SCHEMA_VERSION,
@@ -18,116 +19,18 @@ from .historical_paper_replay import (
     HistoricalTapePoint,
     build_market_bundle,
     collect_resolved_bundles,
+    fetch_batch_price_history,
     fetch_closed_markets,
     fetch_price_history,
     run_mechanical_control,
 )
-from .historical_replay import (
-    HistoricalPricePoint,
-    HistoricalReplay,
-    ReplayResult,
-    REPLAY_SCHEMA_VERSION,
-)
-from .market_model_edge import (
-    MARKET_MODEL_EDGE_SCHEMA_VERSION,
-    MarketModelEdgeObservation,
-    MarketModelEdgeSummary,
-    build_edge_observation,
-    compare_predictions_to_market,
-    select_latest_eligible_price,
-)
-from .market_snapshot import (
-    CANONICAL_SCHEMA_VERSION,
-    DATA_QUALITY_VALUES,
-    MarketLifecycle,
-    MarketSnapshot,
-    PolymarketMarket,
-    PriceObservation,
-    ResolutionMetadata,
-    SnapshotStore,
-    build_snapshot,
-    canonical_json,
-    parse_gamma_market,
-    sha256_canonical,
-    validate_snapshot_transition,
-)
-from .paper_simulation import (
-    PaperFill,
-    PaperInstruction,
-    PaperQuote,
-    PaperRun,
-    PortfolioLedger,
-    SCHEMA_VERSION as PAPER_SIMULATION_SCHEMA_VERSION,
-    simulate_fill,
-)
-from .prediction_ledger import (
-    PREDICTION_LEDGER_SCHEMA_VERSION,
-    PredictionLedger,
-    PredictionProvenance,
-    PredictionRecord,
-    build_prediction,
-)
-from .prediction_council import (
-    PREDICTION_COUNCIL_SCHEMA_VERSION,
-    CouncilRecord,
-    build_council,
-)
+from .historical_replay import HistoricalPricePoint, HistoricalReplay, ReplayResult, REPLAY_SCHEMA_VERSION
+from .market_model_edge import MARKET_MODEL_EDGE_SCHEMA_VERSION, MarketModelEdgeObservation, MarketModelEdgeSummary, build_edge_observation, compare_predictions_to_market, select_latest_eligible_price
+from .market_snapshot import CANONICAL_SCHEMA_VERSION, DATA_QUALITY_VALUES, MarketLifecycle, MarketSnapshot, PolymarketMarket, PriceObservation, ResolutionMetadata, SnapshotStore, build_snapshot, canonical_json, parse_gamma_market, sha256_canonical, validate_snapshot_transition
+from .paper_simulation import PaperFill, PaperInstruction, PaperQuote, PaperRun, PortfolioLedger, SCHEMA_VERSION as PAPER_SIMULATION_SCHEMA_VERSION, simulate_fill
+from .prediction_ledger import PREDICTION_LEDGER_SCHEMA_VERSION, PredictionLedger, PredictionProvenance, PredictionRecord, build_prediction
+from .prediction_council import PREDICTION_COUNCIL_SCHEMA_VERSION, CouncilRecord, build_council
 
 __all__ = [
-    "CALIBRATION_SCHEMA_VERSION",
-    "CalibrationBin",
-    "CalibrationObservation",
-    "CalibrationReport",
-    "CANONICAL_SCHEMA_VERSION",
-    "CLOB_PRICES_HISTORY_URL",
-    "ControlReplayResult",
-    "CouncilRecord",
-    "DATA_QUALITY_VALUES",
-    "HistoricalMarketBundle",
-    "HistoricalTapePoint",
-    "HistoricalPricePoint",
-    "HistoricalReplay",
-    "HISTORICAL_GAMMA_MARKETS_URL",
-    "HISTORICAL_PAPER_REPLAY_SCHEMA_VERSION",
-    "LOG_EPSILON",
-    "MARKET_MODEL_EDGE_SCHEMA_VERSION",
-    "MarketLifecycle",
-    "MarketModelEdgeObservation",
-    "MarketModelEdgeSummary",
-    "MarketSnapshot",
-    "PAPER_SIMULATION_SCHEMA_VERSION",
-    "PREDICTION_COUNCIL_SCHEMA_VERSION",
-    "PREDICTION_LEDGER_SCHEMA_VERSION",
-    "PaperFill",
-    "PaperInstruction",
-    "PaperQuote",
-    "PaperRun",
-    "PolymarketMarket",
-    "PortfolioLedger",
-    "PredictionLedger",
-    "PredictionProvenance",
-    "PredictionRecord",
-    "PriceObservation",
-    "ReplayResult",
-    "ResolutionMetadata",
-    "REPLAY_SCHEMA_VERSION",
-    "SnapshotStore",
-    "build_council",
-    "build_edge_observation",
-    "build_market_bundle",
-    "build_prediction",
-    "build_snapshot",
-    "canonical_json",
-    "collect_resolved_bundles",
-    "compare_predictions_to_market",
-    "fetch_closed_markets",
-    "fetch_price_history",
-    "observe_prediction",
-    "parse_gamma_market",
-    "run_mechanical_control",
-    "score_calibration",
-    "select_latest_eligible_price",
-    "sha256_canonical",
-    "simulate_fill",
-    "validate_snapshot_transition",
+    "CALIBRATION_SCHEMA_VERSION", "CalibrationBin", "CalibrationObservation", "CalibrationReport", "CANONICAL_SCHEMA_VERSION", "CLOB_BATCH_PRICES_HISTORY_URL", "CLOB_PRICES_HISTORY_URL", "ControlReplayResult", "CouncilRecord", "DATA_QUALITY_VALUES", "HistoricalMarketBundle", "HistoricalTapePoint", "HistoricalPricePoint", "HistoricalReplay", "HISTORICAL_GAMMA_MARKETS_URL", "HISTORICAL_PAPER_REPLAY_SCHEMA_VERSION", "LOG_EPSILON", "MARKET_MODEL_EDGE_SCHEMA_VERSION", "MarketLifecycle", "MarketModelEdgeObservation", "MarketModelEdgeSummary", "MarketSnapshot", "PAPER_SIMULATION_SCHEMA_VERSION", "PREDICTION_COUNCIL_SCHEMA_VERSION", "PREDICTION_LEDGER_SCHEMA_VERSION", "PaperFill", "PaperInstruction", "PaperQuote", "PaperRun", "PolymarketMarket", "PortfolioLedger", "PredictionLedger", "PredictionProvenance", "PredictionRecord", "PriceObservation", "ReplayResult", "ResolutionMetadata", "REPLAY_SCHEMA_VERSION", "SnapshotStore", "build_council", "build_edge_observation", "build_market_bundle", "build_prediction", "build_snapshot", "canonical_json", "collect_resolved_bundles", "compare_predictions_to_market", "fetch_batch_price_history", "fetch_closed_markets", "fetch_price_history", "observe_prediction", "parse_gamma_market", "run_mechanical_control", "score_calibration", "select_latest_eligible_price", "sha256_canonical", "simulate_fill", "validate_snapshot_transition",
 ]
